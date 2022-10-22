f982715c6a13493ffbd179b5c4dc7fcc6c44a024
elonehoo <huchengyea@163.com>
2022-10-22 17:01:59 +0800

    fix: update

--- components/DarkToggle.vue
+++ components/DarkToggle.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 const color = useColorMode()
 
 function toggleDark() {
@@ -9,5 +9,20 @@ function toggleDark() {
 <template>
   <button class="!outline-none" @click="toggleDark">
     <div class="dark:i-carbon-moon i-carbon-sun" />
+  </button>
+</template> -->
+
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'dark') : (colorMode.preference = 'light'))
+</script>
+
+<template>
+  <button aria-label="Color Mode" class="inline-block w-5" @click="onClick">
+    <ColorScheme placeholder="...">
+      <Icon v-if="colorMode.value === 'dark'" name="heroicons-outline:moon" class="w-5 h-5" />
+      <Icon v-else name="heroicons-outline:sun" class="w-5 h-5" />
+    </ColorScheme>
   </button>
 </template>
